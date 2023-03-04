8e2e298cbb34d0d8e8a2265b406a826d150ae24d
vignesh096 <123861333+vignesh096@users.noreply.github.com>
2023-03-04 17:36:21 +0530

    Add files via upload

---
+++ jake/joke.js
@@ -0,0 +1,98 @@
+let jokes = [
+  `What do dentists call their x-rays?
+Tooth pics!`,
+
+  `Did you hear about the first restaurant to open on the moon?
+It had great food, but no atmosphere.`,
+
+  `What did one ocean say to the other ocean?
+Nothing, it just waved.`,
+
+  ` Do you want to hear a construction joke?
+Sorry, I'm still working on it.`,
+
+  `Did you hear about the fire at the circus?
+It was in tents!`,
+
+  `Why do ducks have feathers?
+To cover their butt quacks!`,
+
+  `What's the difference between a hippo and a zippo?
+One is really heavy and the other\'s a little lighter.`,
+
+  `at does a nosey pepper do?
+It gets jalapeño business.`,
+
+  `Why should you never trust stairs?
+They're always up to something.`,
+
+  `When does a joke become a 'dad' joke?
+When it becomes apparent.`,
+
+  `Why did the bullet end up losing his job?
+He got fired.`,
+
+  `What kind of shorts do clouds wear?
+Thunderpants`,
+
+  `I entered ten puns in a contest to see which would win.
+No pun in ten did.`,
+
+  `How do you measure a snake?
+In inches—they don't have feet.`,
+
+  `Where does a waitress with only one leg work?
+IHOP.`,
+
+  `What does a house wear?
+Address!`,
+
+  `Why are toilets always so good at poker?
+They always get a flush`,
+
+  ` Why is Peter Pan always flying?
+Because he Neverlands. (I love this joke because it never grows old.)`,
+
+  `You heard the rumor going around about butter?
+Never mind, I shouldn't spread it.`,
+
+  `Two windmills are standing on a wind farm. One asks, 'What's your favorite kind of music?'
+The other replies, 'I'm a big metal fan.'`,
+
+  `The first rule of the Alzheimer's club is…
+Wait, where are we again?`,
+
+  `I took the shell off of my racing snail, thinking it would make him faster.
+But if anything, it made him more sluggish.`,
+  ` What do you get from a pampered cow?
+Spoiled milk.`,
+
+  `How does NASA organize a party?
+They planet.`,
+
+  ` What's the best thing about Switzerland?
+I don't know, but the flag is a big plus.`,
+
+  ` You know, it was so cold in D.C. the other day, I saw a politician with his hands in his own pockets.`,
+  ` How many tickles does it take to get an octopus to laugh?
+Ten tickles`,
+
+  `Why doesn't Dracula have any friends?
+Well, honestly, he's a real pain in the neck.`,
+
+  `yestorday teachers told me I'd never amount to much since I procrastinate so much.
+I told them, “Just you wait!”`,
+
+  `Why were they called the “dark ages?”
+Because there were a lot of knights.`,
+];
+const refresh = () => {
+  let container = document.getElementById("joke");
+  console.log(container);
+  let index = Math.floor(Math.random() * 30);
+  console.log(index);
+  container.innerHTML = `"` + jokes[index] + `"`;
+};
+
+let button = document.getElementById("but");
+button.addEventListener("click", refresh);
